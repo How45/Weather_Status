e865d4e61aabd0d558258a0fe1c192a397c62e2b
Max Letellier <mletellier02@gmail.com>
2022-06-21 23:51:22 +0100

    files

--- Weather.py
+++ Weather.py
@@ -2,8 +2,8 @@ import json
 import urllib.request
 
 def main():
-    PortsmouthWeather = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?lat=50.8036831&lon=-1.075614&appid=05981c208153f8dbbd376121b045ddc4")
-    data = json.loads(PortsmouthWeather.read().decode())
+    area = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?lat=52.1951&lon=0.1313&appid=05981c208153f8dbbd376121b045ddc4") # Cambridge
+    data = json.loads(area.read().decode())
     print(data)
 
     # Delete Unessary jason rows
