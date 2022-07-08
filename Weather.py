a38bd6387e3552d92e34cd1fefe42bb4d89935b7
Max Letellier <mletellier02@gmail.com>
2022-07-08 14:30:09 +0100

    made search feature better & added go back feature

--- Weather.py
+++ Weather.py
@@ -3,24 +3,29 @@ import urllib.request
 from geopy.geocoders import Nominatim
 import tkinter as tk
 
-
 #/------------------------/ WeatherStats Class /------------------------/
 class WeatherStats:
-    def __init__(self, name):
-        self.City = name
+    def __init__(self, name, country):
+        self.area = name
+        self.country = country
 
     def get_Location(self):
-        return self.long, self.lanti
+        return self.lanti, self.long
 
     def form_Data(self):
         geolocator = Nominatim(user_agent="MyApp") # Initialize Nominatim API
-        l = geolocator.geocode(self.City)
+        a = self.area,self.country
+        print(a)
+
+        l = geolocator.geocode(a)
+        print("Found?")
 
         self.long = l.longitude
         self.lanti = l.latitude
 
         area = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=05981c208153f8dbbd376121b045ddc4".format(self.lanti,self.long))
         data = json.loads(area.read().decode())
+        print(data)
 
         remove = ["dt","id","cod"]
         for i in list(data):
@@ -31,21 +36,36 @@ class WeatherStats:
         # Setup self_Stuff
 
 #/------------------------/ UI Function /------------------------/
-def toUiTwo(box, ui1): # This will need to change scence
-    name = box.get()
+def toUiTwo(area,country, ui1): # This will need to change scence
+    name = area.get()
+    area = country.get()
     ui1.destroy()
 
-    wStats = WeatherStats(name)
+    wStats = WeatherStats(name,area)
     UI2(wStats)
 
+def toUiOne(ui2):
+    ui2.destroy()
+    UI1()
+
+#/------------------------/ UI /------------------------/
 def UI2(wStats): # Main Change
     #Class setup
     wStats.form_Data()
+
+    print(wStats.get_Location())
 
     #UI2 Start
     ui2 = tk.Tk()
     ui2.geometry("700x300")
     ui2.title("UI2 Window")
+
+    w = tk.Canvas(ui2, width=250, height=200)
+    w.create_rectangle(0, 0, 100, 100, fill="blue", outline = 'blue')
+    w.pack
+
+    backwards = tk.Button(ui2,height=1, width=12, text="Go Back", command = lambda: toUiOne(ui2))
+    backwards.place(x=0,y=0)
 
     ui2.mainloop()
 
@@ -55,14 +75,25 @@ def UI1():
     ui1.geometry("700x300")
     ui1.title("UI1 Window")
 
-    #textBox
-    box = tk.Entry(ui1, width=15,text="City")
-    box.focus_set()
-    box.place(x=0,y=0)
+    #text
+    Enter = tk.Label(ui1,text="Enter Location:")
+    Enter.place(x=0,y=0)
+
+    #textBox - place
+    box_area = tk.Entry(ui1, width=15)
+    box_area.insert(0,"Area...")
+    box_area.focus_set()
+    box_area.place(x=85,y=0)
+
+    #textBox - country
+    box_county = tk.Entry(ui1, width=15)
+    box_county.insert(0,"Country...")
+    box_county.focus_set()
+    box_county.place(x=183,y=0)
 
     #button
-    comment = tk.Button(ui1,height=1, width=12, text="Enter", command = lambda: toUiTwo(box,ui1))
-    comment.place(x=0,y=20)
+    comment = tk.Button(ui1,height=1, width=12, text="Enter", command = lambda: toUiTwo(box_area,box_county,ui1))
+    comment.place(x=85,y=22)
 
     ui1.mainloop()
 
